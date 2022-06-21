94e539739103312cb94311b070e9d1d418781358
James Pierce <james@superbasic.xyz>
2022-06-21 17:12:56 -0500

    make it a bit more interesting

--- 202206201643/main.js
+++ 202206201643/main.js
@@ -1,4 +1,6 @@
 const matrix = new Array(16).fill().map(a => new Array(16).fill());
+const width = window.innerWidth;
+const height = window.innerHeight;
 
 function lSquare(x, y, size) {
   const hue = frameCount % 255;
@@ -11,10 +13,19 @@ function lSquare(x, y, size) {
   }
 }
 
+function shadowOffset() {
+  const xOffset = -((mouseX - (width / 2)) / 50);
+  const yOffset = -((mouseY - (height / 2)) / 50);
+  return {
+    xOffset,
+    yOffset
+  }
+}
+
 function setup() {
   colorMode(HSB);
   frameRate(30);
-  createCanvas(window.innerWidth, window.innerHeight);
+  createCanvas(width, height);
   background(0);
   noStroke();
 }
@@ -22,13 +33,14 @@ function setup() {
 function draw() {
   clear();
   background(0);
+  const { xOffset, yOffset } = shadowOffset();
   matrix.map((row, rI) => {
     row.map((col, cI) => {
-      const x = rI * 48 + window.innerWidth / 2 - (matrix.length * 24);
-      const y = cI * 48 + window.innerHeight / 2 - (matrix.length * 24);
+      const x = rI * (width / matrix.length) + (width / matrix.length / 4);
+      const y = cI * (height / matrix.length) + (height / matrix.length / 4);
       fill(255);
-      square(Math.sin(frameCount / 8) * 16 + x, Math.cos(frameCount / 8) * 16 + y, 32);
-      lSquare(x, y, 32).draw();
+      square(x + xOffset, y + yOffset, 32);
+      lSquare(x, y, 32 + (abs(rI - matrix.length / 2) + abs(cI - matrix.length / 2))).draw();
     });
   });
 }
